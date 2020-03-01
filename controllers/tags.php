0ec5c11bf303fd8b7544bc13f68ece3939c7979a
Andre Eli <andre.eli@khk.ee>
2020-03-01 21:12:53 +0200

    Add clicking on tags function.

--- controllers/tags.php
+++ controllers/tags.php
@@ -4,7 +4,7 @@
 namespace App;
 
 
-class tags
+class tags extends Controller
 {
     function index(){
         $this -> tags = get_all("SELECT tag_name, COUNT(post_id) AS count FROM post_tags NATURAL JOIN tag GROUP BY tag_id");
