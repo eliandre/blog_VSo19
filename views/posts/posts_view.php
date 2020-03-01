0298ab77482a018ede5bcd2a228655b382246523
Andre Eli <andre.eli@khk.ee>
2020-03-01 21:43:34 +0200

    Comment some lines out.

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,4 +1,4 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<!--<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <style>
@@ -52,14 +52,14 @@
     article h2{color:#333333;}
     h2{color:#34ca78;}
     .margin10{margin-bottom:10px; margin-right:10px;}
-</style>
+</style> -->
 <!------ Include the above in your HEAD tag ---------->
 
 <div class="row">
 
     <div class="col-lg-8">
         <h1><?php echo $post['post_subject'];?></h1>
-        <p class=lead">by <?php echo $post['name'];?></p>
+        <p class="lead">by <?php echo $post['name'];?></p>
         <hr>
         <p>Posted on <?php echo $post['post_created']; ?></p>
         <p>Tags:
